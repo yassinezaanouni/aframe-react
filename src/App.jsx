166dacc97dc83a93f63125499a33901d33760d05
YassineZn <yassinezaanouni@gmail.com>
2023-11-27 17:53:20 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,52 @@
 import "aframe";
 import { Entity, Scene } from "aframe-react";
+import "babel-polyfill";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
   const handleClick = () => {
-    setCount(count + 1);
-    console.log("click");
+    console.log("Clicked!");
+  };
+
+  const handleCollide = () => {
+    console.log("Collided!");
   };
 
   return (
-    <>
-      <h1
-        style={{
-          color: "red",
-          fontSize: "50px",
+    <Scene>
+      <Entity
+        primitive="a-box"
+        color="red"
+        position="0 0 -5"
+        events={{
+          click: handleClick,
+          collide: [handleCollide],
         }}
-      >
-        {count}
-      </h1>
-      <Scene>
+      />
+      {/* add camera */}
+      <Entity primitive="a-camera">
         <Entity
-          primitive="a-box"
-          color="red"
-          position="0 0 -5"
-          events={{
-            click: handleClick,
-          }}
+          primitive="a-cursor"
+          cursor={{ fuse: false }}
+          geometry={{ primitive: "ring", radiusInner: 0.01, radiusOuter: 0.02 }}
+          material={{ color: "white", shader: "flat" }}
+          position="0 0 -1"
         />
-      </Scene>
-    </>
+      </Entity>
+      <Entity
+        text={{
+          value: `welcoem ${count}`,
+          align: "center",
+          color: "red",
+          width: "2.5",
+          height: "2.5",
+        }}
+        position="-1 0.5 -5"
+      />
+    </Scene>
   );
 }
 
